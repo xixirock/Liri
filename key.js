9ad67e1faa7fc82a30fdb6f4e05001ccdb78ff01
xixirock <jlin0402@gmail.com>
2017-11-30 03:10:54 -0800

    added and completed twitter api key registration

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'EqrDq2HW962mHbYfz88C83gZ2',
+  consumer_secret: 'kcUQRB5iGZSjE1iXNCZJiVB3qPfMQltQsPL7YHiMM9RdcUPGag',
+  access_token_key: '936189184976478208-VNf5whTkQF3LUNZR6IKGXQCcON1moUB',
+  access_token_secret: '3oJSNovV4D1ilavXOypm3FQeOFB7AdhBKHaSeibW21C0z',
+}
+
+module.exports = twitterKeys;
