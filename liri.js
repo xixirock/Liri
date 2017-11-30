b1022bdaca1d72216ac8564da2c60f91ce79b5a7
xixirock <jlin0402@gmail.com>
2017-11-30 05:31:12 -0800

    set up basic for liri.js and switch function for commands

--- liri.js
+++ liri.js
@@ -1,22 +1,77 @@
-var Twitter = require('twitter');
+// At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
+var keys = require('./key.js');
+var twitterkeys = key.twitterkeys;
 
-var client = new Twitter({
- consumer_key: '',
- consumer_secret: '',
- access_token_key: '',
- access_token_secret: ''
-});
-
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
- if (!error) {
-   console.log(tweets);
- }
-});
-
+// Set up all necessary variables for the code to propperly run
+var fs = require('fs');
+var twitter = require('twitter');
+var spotify = require('spotify');
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
+
+// Make it so liri.js can take in one of the following commands:
+// my-tweets
+// spotify-this-song
+// movie-this
+// do-what-it-says
+// What Each Command Should Do
+// set up switch for all the different command for users
+
+
+var liriArgv = process.argv[2];
+
+switch(liriArgv) {
+  case "my-tweets": myTweets(); break;
+  case "spotify-this-song": spotifyThisSong(); break;
+  case "movie-this": movieThis(); break;
+  case "do-what-it-says": doWhatItSays(); break;
+
+//show default instruction to users
+
+default: console.log("\r\n" + "type any of the following line right after 'node liri.js' : " + "\r\n" +
+          "1.my-tweets 'any twitter name'" + "\r\n" +
+          "2.spotify-this-song 'any song name'" + "\r\n" +
+          "3.movie-this 'any movie name'" + "\r\n" +
+          "4.do-what-it-says" + "\r\n" +
+          "Make sure to put the movie and songs name in quotation mark if it's more than one word!"
+);
+
+
+
+};
+
+
+
+// node liri.js my-tweets
+// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+// node liri.js spotify-this-song '<song name here>'
+// This will show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
+// Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
+// Step One: Visit https://developer.spotify.com/my-applications/#!/
+// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
+// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
+// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package. See the
+// node liri.js movie-this '<movie name here>'
+// This will output the following information to your terminal/bash window:
+//    * Title of the movie.
+//    * Year the movie came out.
+//    * IMDB Rating of the movie.
+//    * Rotten Tomatoes Rating of the movie.
+//    * Country where the movie was produced.
+//    * Language of the movie.
+//    * Plot of the movie.
+//    * Actors in the movie.
+// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+// It's on Netflix!
+// You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+// node liri.js do-what-it-says
+// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+// Feel free to change the text in that document to test out the feature for other commands.
