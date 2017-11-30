fe678dfad506ef4f2ba42311a25f883ff4654704
xixirock <jlin0402@gmail.com>
2017-11-30 07:44:00 -0800

    added movie and do what it says, still have bug need fixin

--- liri.js
+++ liri.js
@@ -3,8 +3,8 @@ var keys = require('../key.js');
 
 // Set up all necessary variables for the code to propperly run
 var fs = require('fs');
-var twitter = require('twitter');
 var spotify = require('spotify');
+var twitter = require('twitter');
 var request = require('request');
 
 
@@ -33,10 +33,7 @@ default: console.log("\r\n" + "type any of the following line right after 'node 
           "3.movie-this 'any movie name'" + "\r\n" +
           "4.do-what-it-says" + "\r\n" +
           "Make sure to put the movie and songs name in quotation mark if it's more than one word!"
-);
-
-
-
+)
 };
 
 
@@ -68,12 +65,11 @@ function myTweets() {
         data[i].text + "\r\n" + 
         data[i].created_at + "\r\n" + 
         "------------------------------ " + i + " ------------------------------" + "\r\n";
-        console.log(twitterResults);
-        log(twitterResults);
+        console.log(twitterResults);        
       }
     }else{
       console.log("Error: " + error);
-      return
+      return;
     }
   })
 
@@ -98,7 +94,7 @@ function spotifyThisSong() {
 
   params = songName;
 
-  spotify.search({type: "track", query: params }, function(error, data){
+  spotify.search({type: "track", query: params}, function(error, data){
     if(!error){
       var songInfo = data.track.items;
       for(var i = 0; i < 3; i++){
@@ -110,16 +106,15 @@ function spotifyThisSong() {
           "Preview Url: " + songInfo[i].preview_url + "\r\n" + 
           "------------------------------ " + i + " ------------------------------" + "\r\n";
           console.log(spotifyResults);
-          log(spotifyResults); // calling log function
-        }else{
+          }else{
           console.log("Error: " + error);
           return;
         }
       }
     }
-  })
+  });
 
-}
+};
 
 
 
@@ -141,7 +136,27 @@ function spotifyThisSong() {
 //    * Actors in the movie.
 // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 function movieThis() {
+  var movie = process.argv[3];
+  if(!movie){
+    movie = "mr nobody";
+  }
+  params = movie
+  request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var movieObject = JSON.parse(body);
+      var movieResult = 
+      "Title: " + movieObject.Title+"\r\n"+
+      "Year: " + movieObject.Year+"\r\n"+
+      "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+      "Country: " + movieObject.Country+"\r\n"+
+      "Language: " + movieObject.Language+"\r\n"+
+      "Plot: " + movieObject.Plot+"\r\n"+
+      "Actors: " + movieObject.Actors+"\r\n"+
+      "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+      "Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n";
 
+}
+});
 }
 
 
@@ -154,5 +169,12 @@ function movieThis() {
 // Feel free to change the text in that document to test out the feature for other commands.
 
 function doWhatItSays() {
-
-}
+  fs.readFile("random.txt", "utf8", function(error, data){
+    if (!error) {
+      doWhatItSaysResults = data.split(",");
+      spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+    } else {
+      console.log("Error occurred" + error);
+    }
+  });
+};
